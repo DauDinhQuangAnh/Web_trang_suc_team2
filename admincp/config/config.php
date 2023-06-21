7359a4b937175a033b14e3f9d700d959b18e6fd4
Đậu Đình Quang Anh <89402848+DauDinhQuangAnh@users.noreply.github.com>
2023-06-21 22:15:14 +0700

    Delete admincp directory

--- admincp/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $mysqli = new mysqli("localhost","root","","web_trangsuc_sql");
-    // Check connection
-    if ($mysqli->connect_errno) {
-        echo "Kết nối SQL lỗi" . $mysqli->connect_error;
-        exit();
-    }
-?>
--- admincp/modules/menu.php
+++
@@ -1,9 +0,0 @@
-
-<ul class="admincp_list">
-    <li><a href="index.php">Thống Kê</a></li>
-    <li><a href="index.php?action=quanlydanhmucsanpham&query=them">Quản Lý danh mục Sản Phẩm</a></li>
-    <li><a href="index.php?action=quanlysp&query=them">Quản Lý sản phẩm</a></li>
-    <li><a href="index.php?action=quanlydonhang&query=lietke">Quản Lý Đơn Hàng</a></li>
-    <li><a href="modules/quanlyuser/quanlyuser.php">Quản Lý Người Dùng</a></li>
-
-</ul>
